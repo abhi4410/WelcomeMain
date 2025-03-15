36433a95b8c69c9cd2ef84299dd51b1fc6776cbe
ABHISHEK MISHRA <abhishek96240@gmail.com>
2025-03-16 00:47:40 +0530

    contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <div>
       
-      <div className='text-center text-2xl pt-10 border-t'>
+      <div className='text-center text-2xl pt-20 border-t'>
           <Title text1={'CONTACT'} text2={'US'} />
       </div>
 
